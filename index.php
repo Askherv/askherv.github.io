eb50b3104587ae3307c06448c2879ba1050996a9
Nihad Asgarov <askhervn@gmail.com>
2022-05-13 23:53:35 +0300

    edit

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
+<?php
+    ob_start();
+    $user='b211210554@gmail.com';
+    $pass='98716206237';
+
+    if(isset($_POST['kullaniciadi']))
+    {
+        $kullaniciadi=$_POST['kullaniciadi'];
+        $sifre=$_POST['sifre'];
+
+        if ($kullaniciadi==$user && $sifre==$pass){
+            header('location:hakkinda.html');
+        }
+        else 
+        {
+            echo 'E-Posta veya şifre hatalı';
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login-Bakü'den bir genç</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -11,15 +30,12 @@
     <div class="main">
         <div class="form-box">
             <h3><span class="ilk-harf">B</span>akü'den bir genç</h3>
-            <form id="Login" class="input-group" action="hakkinda.html" method="post" name="form" onsubmit="return validated()">
+            <form id="Login" class="input-group" action="hakkinda.html" method="post">
                 <input type="text" class="input-field" name="kullaniciadi" placeholder="Kullanıcı adınızı giriniz" required>
-                <div id="kullaniciadi_error"></div>
                 <input type="password" class="input-field" name="sifre" placeholder="Şifrenizi giriniz" required>
-                <div id="sifre_error"></div>
                 <button type="submit" class="submit-btn">Giriş Yap</button>
             </form>
         </div>
     </div>
-    <script src="valid.js"></script>
 </body>
 </html>
